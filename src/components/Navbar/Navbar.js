363a7bb9ac08725354d7edbdbe4e7b64e94023a0
Alejandro Centeno <alejocm21@gmail.com>
2023-03-06 23:19:28 -0300

    search by name

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { searchVideogame } from "../../redux/action";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState("");
+
+/*   useEffect(() => {
+    dispatch(getVideogames());
+  }, [dispatch]); */
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    dispatch(searchVideogame(search));
+    setSearch("")
+  };
+
   return (
     <div>
-    <nav className="navbar bg-dark navbar-dark fixed-top">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/home">VideoJuegos</Link>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-dark" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-  </div>
-  )
-}
+      <nav className="navbar bg-dark navbar-dark fixed-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/home">
+            VideoJuegos
+          </Link>
+          <form onClick={e => handleSearch(e)} className="d-flex" role="search">
+            <input
+              onChange={e => handleSubmit(e)}
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-dark" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const GET_ALL_VIDEOGAMES = "GET_ALL_VIDEOGAMES";
 export const GET_VIDEOGAME_BY_ID = "GET_VIDEOGAME_BY_ID";
+export const SEARCH_VIDEOGAME = "SEARCH_VIDEOGAME";
 
 export const getVideogames = () => {
   return async function(dispatch) {
@@ -18,5 +19,12 @@ export const getVideogameById = (id) => {
   return async function(dispatch) {
     const { data: videogameById } = await axios(`http://localhost:3001/detail-game/${id}`)
     return dispatch({ type: GET_VIDEOGAME_BY_ID, payload: videogameById })
+  }
+};
+
+export const searchVideogame = (name) => {
+  return async function(dispatch) {
+    const { data: searchByName } = await axios(`http://localhost:3001/videogames?name=${name}`)
+    return dispatch({ type: SEARCH_VIDEOGAME, payload: searchByName })
   }
 }
