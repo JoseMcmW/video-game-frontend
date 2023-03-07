f354eb29e879155ddafd6ec452e7496ce87fe365
Jose Centeno <josemanuelcm25@gmail.com>
2023-03-07 19:04:21 -0300

    link navbar-brand to home, reset search input, search videogames upperCase and lowerCase

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { searchVideogame } from "../../redux/action";
+import { getVideogames } from "../../redux/action";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const [search, setSearch] = useState("");
 
-/*   useEffect(() => {
+  useEffect(() => {
     dispatch(getVideogames());
-  }, [dispatch]); */
+  }, [dispatch]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setSearch(event.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
   };
 
-  const handleSearch = (event) => {
-    event.preventDefault();
+  const handleSearch = (e) => {
+    e.preventDefault();
     dispatch(searchVideogame(search));
-    setSearch("")
+    setSearch("");
   };
+
+  const goToHome = (e) => {
+    e.preventDefault()
+    dispatch(getVideogames());
+  }
 
   return (
     <div>
       <nav className="navbar bg-dark navbar-dark fixed-top">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/home">
+          <Link className="navbar-brand" to="/home" onClick={goToHome}>
             VideoJuegos
           </Link>
-          <form onClick={e => handleSearch(e)} className="d-flex" role="search">
+          <form onSubmit={handleSearch} className="d-flex" role="search">
             <input
-              onChange={e => handleSubmit(e)}
+              onChange={handleSubmit}
               className="form-control me-2"
               type="search"
-              placeholder="Search"
+              placeholder="Search Videogame"
               aria-label="Search"
+              value={search}
             />
-            <button className="btn btn-dark" type="submit">
+            <button className="btn btn-dark" type="submit" value="submit">
               Search
             </button>
           </form>
